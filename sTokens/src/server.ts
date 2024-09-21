3baf6a203b3922f844ce94c7baf49983953ef604
Parth Gupta <parth.eng1210@gmail.com>
2024-09-21 18:00:45 +0530

    added hyperlane bridge

--- sTokens/src/server.ts
+++ sTokens/src/server.ts
@@ -1,11 +1,29 @@
-import { ActionConfirmationStatus } from "@stackr/sdk";
+import {ActionConfirmationStatus, ActionSchema, AllowedInputTypes} from "@stackr/sdk";
 import express, { Request, Response } from "express";
 import { stokenMachine } from "./stackr/machine";
 import { mru } from "./stackr/mru";
 import { schemas } from "./stackr/schemas";
 import { transitions } from "./stackr/transitions";
+import {Bridge} from "@stackr/sdk/plugins";
+import {AbiCoder, formatEther, Wallet} from "ethers";
+
+const signMessage = async (
+    wallet: Wallet,
+    schema: ActionSchema,
+    payload: AllowedInputTypes
+) => {
+  const signature = await wallet.signTypedData(
+      schema.domain,
+      schema.EIP712TypedData.types,
+      payload
+  );
+  return signature;
+};
 
 const PORT = 3210;
+
+const abiCoder = AbiCoder.defaultAbiCoder();
+export const operator = new Wallet(process.env.PRIVATE_KEY as string);
 
 export async function setupServer() {
   const app = express();
@@ -90,4 +108,35 @@ export async function setupServer() {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+
+  Bridge.init(mru, {
+    handlers: {
+      BRIDGE_TOKEN: async (args) => {
+        console.log("args data:", args.data);
+        const [_token, _to, _amount] = abiCoder.decode(
+            ["address", "address", "uint256"],
+            args.data
+        );
+        console.log("Minting token to", _to, "with amount", Number(_amount));
+        const inputs = {
+          address: _to,
+          amount: Number(formatEther(_amount)),
+        };
+
+        console.log("inputs:", inputs);
+        const signature = await signMessage(operator, schemas.MintSchema, inputs);
+        const action = schemas.MintSchema.actionFrom({
+          inputs,
+          signature,
+          msgSender: operator.address,
+        });
+
+        return {
+          transitionName: "mint",
+          action: action,
+        };
+      },
+    },
+  });
+
 }
